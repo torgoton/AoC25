743ebef93b38e40201eef7003939182118a8295b
Chris Schumann <chris@idlelion.net>
2025-12-02 13:22:42 -0600

    Day 2, part 2

--- day02-gift-shop.rb
+++ day02-gift-shop.rb
@@ -2,9 +2,10 @@
 # Gift Shop
 
 input_file = ARGV[0]
+any_length = !!ARGV[1]
 
 if input_file.nil?
-  puts "Usage: ruby #{__FILE__} <input_file>"
+  puts "Usage: ruby #{__FILE__} <input_file> [any_length]"
   exit 1
 end
 
@@ -12,6 +13,30 @@ puts "Looking for invalid IDs in: #{input_file}"
 
 @invalid_count = 0
 @invalid_sum = 0
+
+def check_valid(id, any_length: false)
+  l = id.length
+  mid = l / 2
+  if !any_length
+    if l.even?
+      if id[0...mid] == id[mid..-1]
+        @invalid_count += 1
+        @invalid_sum += id.to_i
+        puts "Invalid ID found: #{id}"
+      end
+    end
+    return
+  end
+  (1..(mid)).each do |sample|
+    target = id[0...sample]
+    if id == target * (l / sample)
+      @invalid_count += 1
+      @invalid_sum += id.to_i
+      puts "Invalid ID found: #{id} (repeats #{target})"
+      return
+    end
+  end
+end
 
 File.open(input_file, 'r') do |f|
   input = f.readline
@@ -26,15 +51,7 @@ puts "Ranges: #{@ranges.inspect}"
 @ranges.each do |r|
   r.each do |id|
     s = id.to_s
-    l = s.length
-    if l.even?
-      mid = l / 2
-      if s[0...mid] == s[mid..-1]
-        @invalid_count += 1
-        @invalid_sum += id
-        puts "Invalid ID found: #{id}"
-      end
-    end
+    check_valid(s, any_length: any_length)
   end
 end
 
